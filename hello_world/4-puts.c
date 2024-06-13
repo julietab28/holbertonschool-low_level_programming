c15ad50c075ed329f7fa386cfc0eb8e6942d79b9
root <9301@holbertonstudents.com>
2024-06-13 17:03:29 +0000

    jejej

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-	char Frase[] = "\"Programming is like building a multilingual puzzle";
-	puts(Frase);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
