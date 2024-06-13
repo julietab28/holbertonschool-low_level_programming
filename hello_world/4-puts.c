cfb7c0e0f47938449b8697048f5bbac50e0bb832
root <9301@holbertonstudents.com>
2024-06-13 16:46:52 +0000

    LO LOGREEE SOLAAA

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - la main mani
+ *
+ * Return: Always 0 (esitosamente)
+ */
+int main()
+{
+	char Frase[] = "Programming is like building a multilingual puzzle \n";
+	
+	puts(Frase);
+	
+	return (0);
+}
