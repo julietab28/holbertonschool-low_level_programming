266fbd7dfd9e76f3bdc1cbd7a4af584cd3a997e9
root <9301@holbertonstudents.com>
2024-06-17 01:10:34 +0000

    low and upper

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	char x;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	putchar(i);
+	}
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
