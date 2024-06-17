d170d8e35c69ccd0bf0e931878f365f2a40a6574
root <9301@holbertonstudents.com>
2024-06-17 13:37:55 +0000

    e16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+	char x;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	putchar(i);
+	}
+
+	for (x = 'a'; x <= 'f'; x++)
+	{
+	putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
