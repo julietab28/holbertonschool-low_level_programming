9f2d1ca00bd99826d79d67bfe365da2004c67b7e
root <9301@holbertonstudents.com>
2024-06-18 17:17:01 +0000

    ejercicio6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - jjjj
+ *
+ * @n: numero
+ *
+ * Return: valor absoluto
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
