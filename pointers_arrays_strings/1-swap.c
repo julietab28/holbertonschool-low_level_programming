8ffe7db5552bb8e4ec95c01202a232b849c219b6
root <9301@holbertonstudents.com>
2024-06-19 17:25:06 +0000

    exe1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code
+ * @a: pointer1
+ * @b: pointer2
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
