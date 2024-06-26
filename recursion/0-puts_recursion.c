21aa55b94bacaf2ed214deb5a0fa7da91a2c94a4
root <you@example.com>
2024-06-26 14:24:29 +0000

    exe0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - retornar un string
+ * @s: string
+ * Return: string
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	_putchar('\n');
+
+	return;
+}
