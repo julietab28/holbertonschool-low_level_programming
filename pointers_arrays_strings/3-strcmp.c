80c8d6f6bdbba9a57534e17fa0e6a352a3fad83f
root <you@example.com>
2024-06-26 01:33:49 +0000

    3exe

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] == '\0' && s2[i] != '\0')
+		if (s1[i] < s2[i])
 		{
 			return (-s2[i]);
 		}
-		else if (s1[i] != '\0' && s2[i] == '\0')
+		else if (s1[i] > s2[i])
 		{
 			return (s2[i]);
 		}
