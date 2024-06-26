c6e232d76e9356bc13424e03983957503481f2d5
root <you@example.com>
2024-06-26 14:55:08 +0000

    exe0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,8 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*s);
-	_putchar('\n');
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-	return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
