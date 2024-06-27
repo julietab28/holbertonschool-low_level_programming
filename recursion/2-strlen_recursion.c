93052074295a15a659cf86f9d7c354b34ce227c4
root <you@example.com>
2024-06-27 18:52:28 +0000

    terminar

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - check the code
+ * @s: char
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s)
+	{
+		s + 1;
+		n = s;
+	}
+}
