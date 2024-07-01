a646d6f1f761250ed8ef92472d816526eb88796f
root <you@example.com>
2024-07-01 19:43:43 +0000

    arg1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - cantidad de argumentosss
+ * @argc: int
+ * @argv: char
+ * Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d, %s\n", i, argv[i]);
+	}
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+argc y argv
