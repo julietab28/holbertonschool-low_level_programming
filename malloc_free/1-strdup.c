fc2a56a4d0507877acf46f15ab7553854862c5b5
root <you@example.com>
2024-07-02 18:24:02 +0000

    ejrecicio1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - check
+ * @str: array
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	int a, b;
+	char *str_2;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+}
