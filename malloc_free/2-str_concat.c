17cd253eef7689dd5e6ad92681f5ef94117aba38
root <you@example.com>
2024-07-03 23:04:54 +0000

    concatenar dos strings

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenar s1 con s2
+ * @s1: string 1
+ * @s2: string2
+ * Return: Always 0.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2;
+	char *str_concat;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+
+	for (len1 = 0; s1[len1] == '\0'; len1++)
+	{
+	}
+
+	for (len2 = 0; s2[len2] == '\0'; len2++)
+	{
+	}
+
+	str_concat = malloc((len1 - 1) + len2 * sizeof(char));
+
+	if (str_concat == NULL)
+	{
+		return (NULL);
+	}
+
+	return (str_concat);
+}
