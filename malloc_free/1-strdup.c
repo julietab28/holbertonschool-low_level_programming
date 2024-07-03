65e5dbe49a068f47d228e3ff30e00dc79a937a66
root <you@example.com>
2024-07-03 22:46:54 +0000

    copiar contenido de str a str2

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,18 +3,37 @@
 #include <stdlib.h>
 
 /**
- * _strdup - check
- * @str: array
+ * _strdup - copiar una cadena de caracteres a otra
+ * @str: cadena de caracteres a copiar
  * Return: Always 0.
  */
 char *_strdup(char *str)
 {
-	int a, b;
+	int len, i;
 	char *str_2;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+
+	}
+		str_2 = malloc(len + 1 * sizeof(char));
+
+	if (str_2 == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str_2[i] = str[i];
+	}
+		i++;
+		str_2[i] = '\0';
+
+	return (str_2);
 }
