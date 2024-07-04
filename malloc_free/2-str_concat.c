bec5cc50cb98dfc7a1ea5337dba5f602d63cba25
root <you@example.com>
2024-07-04 18:22:54 +0000

    concatenar dos strings

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	}
 
-	for (len1 = 0; s1[len1] <= '\0'; len1++)
+	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{
 	}
 	for (len2 = 0; s2[len2] != '\0'; len2++)
