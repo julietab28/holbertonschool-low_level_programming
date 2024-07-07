75f99ecd2bdce3bee84c7b6af0038b888a1f6bbd
root <you@example.com>
2024-07-07 00:15:36 +0000

    terminar

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - 
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+	int w, h;
+	int *grid
+
+	if (width <= 0)
+	{
+		return (NULL);
+	}
+
+	if (height <= 0)
+	{
+		return (NULL);
+	}
+
+	for (w = 0; w < width; w++)
+	{
+		grid[w] = 
+	}
+	for (h = 0; h < height; h++)
+	{
+	}
+
+	
+}
