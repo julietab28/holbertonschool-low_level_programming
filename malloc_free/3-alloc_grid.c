632056440e67a2112c7ccbe23c56d5958289bcb9
root <you@example.com>
2024-07-08 23:51:03 +0000

    prueba

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -3,16 +3,15 @@
 #include <stdlib.h>
 
 /**
- * **alloc_grid - 
+ * **alloc_grid - 2D array
  * @width: width of the grid
  * @height: height of the grid
- *
- * Return: Nothing.
+ * Return: grid
  */
 int **alloc_grid(int width, int height)
 {
 	int w, h;
-	int *grid
+	int **grid;
 
 	if (width <= 0)
 	{
@@ -24,13 +23,21 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (w = 0; w < width; w++)
-	{
-		grid[w] = 
-	}
+	grid = malloc(sizeof(int *) * height);
+
 	for (h = 0; h < height; h++)
 	{
+		grid[h] = malloc(sizeof(int) * width);
 	}
 
-	
+	for (h = 0; h < height; h++)
+	{
+		for (w = 0; w < width; w++)
+		{
+			grid[w][h] = 0;
+		}
+	}
+
+	return (grid);
+
 }
