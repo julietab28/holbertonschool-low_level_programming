8581582e5921bda00424e7b72a09c1746ae30d58
root <you@example.com>
2024-07-10 16:48:56 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - define una estructura dog con diferentes tipos de datos
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
