9e484ed179f777b9e6096e815e19e59e494dcffa
juli <you@example.com>
2024-07-11 17:44:12 +0000

    dog2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "dog.h"
+
+/**
+ * print_dog - check the code
+ * @d: dog
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name == NULL || d->owner == NULL)
+	{
+		printf("nil");
+	}
+}
