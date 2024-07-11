40c933fd6b9b8c83f6b91483a4977c1366f85d19
juli <you@example.com>
2024-07-11 18:40:58 +0000

    dog3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,10 +17,11 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 struct dog_t
 {
 	char *name;
 	float age;
-	char *owner
+	char *owner;
 };
 #endif
