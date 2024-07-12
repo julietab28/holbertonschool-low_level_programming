a68332845c937cb17c8875d54337bb85ed450298
root <you@example.com>
2024-07-12 18:49:50 +0000

    terminar

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,5 +8,12 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	if (name == NULL || owner == NULL)
+	{
+		
+	}
+	for (len1 = 0; name[len1] != '\0'; len1++)
+	{
+	}
 }
 
---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+
+}
