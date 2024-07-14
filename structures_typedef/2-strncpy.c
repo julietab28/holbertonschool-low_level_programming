d9cf46a0e72b65c1cb4b086356f21ff29b03affa
root <you@example.com>
2024-07-14 19:00:32 +0000

    pppperritonuevo

---
+++ structures_typedef/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strncpy - check the code
+ * @dest: char
+ * @src: char
+ * @n: entero
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,19 +1,50 @@
 #include <stdio.h>
 #include "dog.h"
+#include "2-strncpy.c"
 #include <stdlib.h>
 /**
- * main - check the code
- *
+ * new_dog - funcion para crear un nuevo perro
+ * @name: char
+ * @age: float
+ * @owner: char
  * Return: Always 0.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (name == NULL || owner == NULL)
+	dog_t *perrito;
+	char *nombre, *duenio;
+	int len1, len2;
+
+	perrito = malloc(sizeof(dog_t));
+	if (perrito == NULL)
 	{
-		
+		free(perrito);
+		return (NULL);
 	}
+
 	for (len1 = 0; name[len1] != '\0'; len1++)
+	for (len2 = 0; owner[len2] != '\0'; len2++)
+
+	nombre = malloc(sizeof(name));
+	if (nombre == NULL)
 	{
+		free(perrito);
+		return (NULL);
 	}
+
+	duenio = malloc(sizeof(owner));
+	if (duenio == NULL)
+	{
+		free(perrito);
+		return (NULL);
+	}
+
+	perrito = malloc(sizeof(char) * (len1 + len2));
+
+	_strncpy(nombre, name, len1);
+	perrito->age = age;
+	_strncpy(duenio, owner, len2);
+
+	return (perrito);
 }
 
