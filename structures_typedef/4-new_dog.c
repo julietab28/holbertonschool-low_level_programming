d1cbcca0b4f2ce6cfb35802fee6f2001d88f92ae
root <you@example.com>
2024-07-14 21:13:01 +0000

    dog nuevo

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,19 +15,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *nombre, *duenio;
 	int len1, len2;
 
-	perrito = malloc(sizeof(dog_t));
-	if (perrito == NULL)
-	{
-		return (NULL);
-	}
-
 	for (len1 = 0; name[len1] != '\0'; len1++)
 	for (len2 = 0; owner[len2] != '\0'; len2++)
 
 	nombre = malloc(sizeof(len1));
 	if (nombre == NULL)
 	{
-		free(perrito);
 		return (NULL);
 	}
 
@@ -35,12 +28,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (duenio == NULL)
 	{
 		free(nombre);
-		free(perrito);
 		return (NULL);
 	}
 
 	_strncpy(nombre, name, len1);
 	_strncpy(duenio, owner, len2);
+	perrito = malloc(sizeof(dog_t));
+	if (perrito == NULL)
+	{
+		free(nombre);
+		free(duenio);
+		return (NULL);
+	}
 	perrito->name = nombre;
 	perrito->age = age;
 	perrito->owner = duenio;
