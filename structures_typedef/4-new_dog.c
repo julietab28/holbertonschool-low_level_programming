c0fa6bf6665a59bbca7ffb89fe85539a44e7efe1
root <you@example.com>
2024-07-14 21:57:10 +0000

    dog nuevo

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "dog.h"
 #include "2-strncpy.c"
-#include <string.h>
 #include <stdlib.h>
 /**
  * new_dog - funcion para crear un nuevo perro
@@ -14,8 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *perrito;
 	char *nombre, *duenio;
-	int len1 = strlen(name);
-	int len2 = strlen(owner);
+	int len1, len2;
+
+	for (len1 = 0; name[len1] != '\0'; len1++)
+	for (len2 = 0; owner[len2] != '\0'; len2++)
 
 	nombre = malloc(sizeof(char) * (len1 + 1));
 	if (nombre == NULL)
@@ -32,7 +33,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	_strncpy(nombre, name, len1);
 	nombre[len1] = '\0';
-	_strncpy(duenio, owner, len2 + 1);
+	_strncpy(duenio, owner, len2);
 	duenio[len2] = '\0';
 	perrito = malloc(sizeof(dog_t));
 	if (perrito == NULL)
