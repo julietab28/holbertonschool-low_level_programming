02731c2cda953e655bd3ed59c717cc6c26b6a43a
root <you@example.com>
2024-07-15 18:25:58 +0000

    iteracion

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - prints an integer
+ * @array: the integer to print
+ * @size: size_t
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		(action)(array[i]);
+	}
+}
+
