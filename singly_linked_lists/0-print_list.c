e42ee5b7aca8c56a62e0005c6ae9e2f26f0f6718
root <you@example.com>
2024-08-05 20:06:13 +0000

    Singly Linked List - Exe1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,4 @@
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
@@ -10,5 +9,18 @@
  */
 size_t print_list(const list_t *h)
 {
+	int i;
 
+	for (i = 0; h != NULL; i++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{	printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (i);
 }
